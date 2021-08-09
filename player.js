258a54cff03be38324dfe1d38ba9fae61f881934
daniel abrgel <danielabrgel91@gmail.com>
2021-08-09 16:25:45 +0300

    css/html fixes

--- player.js
+++ player.js
@@ -3,9 +3,11 @@ let jq = document.createElement('script');
 jq.src = "https://code.jquery.com/jquery-3.5.0.js";
 document.getElementsByTagName('head')[0].appendChild(jq);
 
+
+
 const getTooltipStr = (content,id,classes)=>{
 
-    let html =`<div id="x_${id}" style="display:block; color:white"  class="sttip ${classes}">
+    let html =`<div id="x_${id}" style="display: none; color: rgb(244, 237, 237); position: absolute; bottom: 50px; left: 1100px; background: black; border-block-color: inherit;">
    	<div class="tooltip in">
 		<div class="tooltip-arrow"></div>
 		<div class="tooltip-arrow second-arrow"></div>
@@ -17,22 +19,23 @@ const getTooltipStr = (content,id,classes)=>{
     return html
 }
 
-
+// To make the load sync
 setTimeout(function() {
 
 //inject css
-    var linkElement = document.createElement('link');
+    let linkElement = document.createElement('link');
     /* add attributes */
     linkElement.setAttribute('rel', 'stylesheet');
     linkElement.setAttribute('href', 'https://guidedlearning.oracle.com/player/latest/static/css/stTip.css');
     /* attach to the document head */
     document.getElementsByTagName('head')[0].appendChild(linkElement);
-    $("head").append(linkElement).append(function(){
+    $("head").append(linkElement)
 
 
 
 ///ajax get all GUID
-        const url = "https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867"
+    let leftPos = 400
+    const url = "https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867"
         $(document).ready(()=>{
             $.ajax({
                 url:url,
@@ -53,16 +56,13 @@ setTimeout(function() {
                         const formEl = document.body
                         formEl.append(divEl)
                         console.log(`#x_${step.id}`)
-                        if(placement === 'right'){
+                        if(content.includes('Welcome')){
                             $(`#x_${step.id}`).css({
                                 position: "absolute",
-                                right: 70,
-                                top: 20
-                            })
-                        }else if(placement === 'bottom'){
-                            $(`#x_${step.id}`).css({
-                                position: "absolute",
-                                bottom: 50
+                                display: "block",
+                                bottom: '',
+                                left: 800,
+                                top: 70
                             })
                         }
                         $(selector).mouseenter(()=>{
@@ -74,9 +74,7 @@ setTimeout(function() {
                             $(`#x_${step.id}`).hide();
                         })
                     })
-
                 }
             })
         })
-    })
-},2000);
+},3000);
