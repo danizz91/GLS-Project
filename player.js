4b9eff4df67ccf04db6325c1e6e55dbd12151e4e
daniel abrgel <danielabrgel91@gmail.com>
2021-08-10 12:00:23 +0300

    final commit last commit.

--- player.js
+++ player.js
@@ -1,13 +1,24 @@
 //Inject Jquery
-let jq = document.createElement('script');
-jq.src = "https://code.jquery.com/jquery-3.5.0.js";
-document.getElementsByTagName('head')[0].appendChild(jq);
+
+main();
+
+ async function main() {
+
+     await loadJquery();
+     loadCss();
+     createSteps();
+     console.log('player.js has loaded!')
+ }
 
 
+const getTooltipStr = (content,id,classes,placement)=>{
+    let vertical = "top"
+    if(placement === 'bottom'){
+        vertical = "bottom"
+        placement = "left"
+    }
 
-const getTooltipStr = (content,id,classes)=>{
-
-    let html =`<div id="x_${id}" style="display: none; color: rgb(244, 237, 237); position: absolute; bottom: 50px; left: 1100px; background: black; border-block-color: inherit;">
+    let html =`<div id="x_${id}" style="display: none; color: rgb(244, 237, 237); position: absolute;${placement}:${parseInt(id)*10}%;${vertical}:10%;border-radius:30px; padding:10px; background: black; border-block-color: inherit;">
    	<div class="tooltip in">
 		<div class="tooltip-arrow"></div>
 		<div class="tooltip-arrow second-arrow"></div>
@@ -20,9 +31,42 @@ const getTooltipStr = (content,id,classes)=>{
 }
 
 // To make the load sync
-setTimeout(function() {
 
-//inject css
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+ async function loadJquery(){
+
+    let jq = document.createElement('script');
+    jq.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js";
+    document.getElementsByTagName('head')[0].appendChild(jq);
+    // waiting for jQuery to load.
+    while(typeof(window.jQuery) === undefined ||  typeof(window.$)===undefined || typeof($.ajax)!=="function"){
+        await sleep(300)
+    }
+
+    console.log("jQuery loaded.")
+
+ }
+
+
+ function  loadCss(){
+     //inject css
+     let linkElement = document.createElement('link');
+     /* add attributes */
+     linkElement.setAttribute('rel', 'stylesheet');
+     linkElement.setAttribute('href', 'https://guidedlearning.oracle.com/player/latest/static/css/stTip.css');
+     /* attach to the document head */
+     document.getElementsByTagName('head')[0].appendChild(linkElement);
+     $("head").append(linkElement)
+ }
+
+
+function createSteps(){
+    //inject css
     let linkElement = document.createElement('link');
     /* add attributes */
     linkElement.setAttribute('rel', 'stylesheet');
@@ -34,47 +78,44 @@ setTimeout(function() {
 
 
 ///ajax get all GUID
-    let leftPos = 400
     const url = "https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867"
-        $(document).ready(()=>{
-            $.ajax({
-                url:url,
-                dataType:'jsonp',
-                success:(data)=>{
-                    const jsonData = data.data.structure.steps
-                    jsonData.forEach((step)=>{
-                        if(JSON.stringify(step.action.contents) === undefined){
-                            return
-                        }
-                        let content = (JSON.stringify(step.action.contents).split(":")[1]).slice(0,-4).slice(1)
-                        let selector = step.action.selector
-                        let placement = step.action.placement
-                        const html = getTooltipStr(content,step.id, step.action.classes)
-                        const divEl = document.createElement('div')
-                        divEl.innerHTML = html
-                        selector = selector.replaceAll(/"/g,"'")
-                        const formEl = document.body
-                        formEl.append(divEl)
-                        console.log(`#x_${step.id}`)
-                        if(content.includes('Welcome')){
-                            $(`#x_${step.id}`).css({
-                                position: "absolute",
-                                display: "block",
-                                bottom: '',
-                                left: 800,
-                                top: 70
-                            })
-                        }
-                        $(selector).mouseenter(()=>{
-                            console.log('show')
-                            $(`#x_${step.id}`).show();
-                        })
-                        $(selector).mouseleave(()=>{
-                            console.log('hide')
-                            $(`#x_${step.id}`).hide();
-                        })
+
+    $.ajax({
+        url:url,
+        dataType:'jsonp',
+        success:(data)=>{
+
+            const jsonData = data.data.structure.steps
+            jsonData.forEach((step)=>{
+                if(JSON.stringify(step.action.contents) === undefined){
+                    return
+                }
+
+                let content = (JSON.stringify(step.action.contents).split(":")[1]).slice(0,-4).slice(1)
+                let selector = step.action.selector
+                let placement = step.action.placement
+                const html = getTooltipStr(content,step.id, step.action.classes,placement)
+                const divEl = document.createElement('div')
+                divEl.innerHTML = html
+                selector = selector.replaceAll(/"/g,"'")
+                const formEl = document.body
+                formEl.append(divEl)
+                if(content.includes('Welcome')){
+                    $(`#x_${step.id}`).css({
+                        position: "absolute",
+                        display: "block",
+                        bottom: '',
+                        right: 300,
+                        top: 70
                     })
                 }
+                $(selector).mouseenter(()=>{
+                    $(`#x_${step.id}`).show();
+                })
+                $(selector).mouseleave(()=>{
+                    $(`#x_${step.id}`).hide();
+                })
             })
-        })
-},3000);
+        }
+    })
+}
